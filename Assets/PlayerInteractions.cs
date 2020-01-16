3312f07ba14290e9520536324d27092300c01667
Tony Yeon <tony.yeon8310@gmail.com>
2020-01-16 08:40:40 -0800

    added mouse input for attack

--- Assets/PlayerInteractions.cs
+++ Assets/PlayerInteractions.cs
@@ -4,22 +4,48 @@ using UnityEngine;
 
 public class PlayerInteractions : MonoBehaviour
 {
-    [Header("Melee Attack Attributes")]
     public Rigidbody enemy;
+    [Header("Melee Attack Attributes")]
     public float meleeAttackDistance;
-    
-    private RaycastHit enemyDetected;
+    [Header("Ranged Attack Attributes")]
+    public float rangedAttackDistance;
 
+    private RaycastHit enemyDetected;
+    private PlayerInput inputActions;
+    private float attackDetection;
+
+    private void Awake() {
+        inputActions = new PlayerInput();
+        inputActions.Player.General.performed += ctx => attackDetection = ctx.ReadValue<float>();
+    }
     void FixedUpdate()
     {
-        // Melee Attack Detection
         CapsuleCollider playerCollider = gameObject.GetComponent<CapsuleCollider>();
         Vector3 playerColliderCenter = playerCollider.transform.position;
         playerColliderCenter.y += playerCollider.height/2f;
+        // Melee Attack Detection
         if (Physics.Raycast(playerColliderCenter, playerCollider.transform.forward, out enemyDetected, meleeAttackDistance))
         {
-            Debug.Log(enemyDetected);
+            //Debug.Log(enemyDetected);
         }
-        Debug.DrawRay(playerColliderCenter, playerCollider.transform.forward, Color.red);
+        //Debug.DrawRay(playerColliderCenter, playerCollider.transform.forward + new Vector3(0,0,1), Color.red);
+
+        // Ranged Attack Detection
+        if (Physics.Raycast(playerColliderCenter, playerCollider.transform.forward, out enemyDetected, rangedAttackDistance))
+        {
+            if (enemyDetected.transform.gameObject.tag == "Enemy" && attackDetection == 1)
+            {
+                Debug.Log(enemyDetected.collider);
+            }
+        }
+        //Debug.DrawRay(playerColliderCenter, playerCollider.transform.forward + new Vector3(0,0,4), Color.blue);
+    }
+	private void OnEnable() 
+    {
+        inputActions.Enable();
+    }
+    private void OnDisable() 
+    {
+        inputActions.Disable();
     }
 }
